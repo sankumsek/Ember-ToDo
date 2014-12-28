37831286007dcfd3e555b5085abd11488a69c74f
Sanjeev Sekar <sankumsek@gmail.com>
2014-12-27 17:43:57 -0800

    Delete extra files

--- js/libs/data-master/packages/activemodel-adapter/lib/system.js
+++
@@ -1,7 +0,0 @@
-import ActiveModelAdapter from "activemodel-adapter/system/active_model_adapter";
-import ActiveModelSerializer from "activemodel-adapter/system/active_model_serializer";
-
-export {
-  ActiveModelAdapter,
-  ActiveModelSerializer
-};
--- js/libs/data-master/packages/ember-data/lib/system/debug.js
+++
@@ -1,8 +0,0 @@
-/**
-  @module ember-data
-*/
-
-import "ember-data/system/debug/debug_info";
-import DebugAdapter from "ember-data/system/debug/debug_adapter";
-
-export default DebugAdapter;
