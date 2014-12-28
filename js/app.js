653a0f24aab65cedd35961a3f14301571ddb65de
Sanjeev Sekar <sankumsek@gmail.com>
2014-12-27 17:43:23 -0800

    Delete extra files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 window.Todos = Ember.Application.create();
 // Creates new instance of Ember Application
 
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+// Indicates that ApplicationAdapter is an extension of FixtureAdapter
