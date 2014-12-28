3a1ada0b1c15cd586c8cb217dbcac9a28f3f5b54
Sanjeev Sekar <sankumsek@gmail.com>
2014-12-27 18:01:32 -0800

    Create TodosRoute class

--- js/router.js
+++ js/router.js
@@ -2,3 +2,8 @@ Todos.Router.map(function() {
 	this.resource('todos', {path: '/' })
 });
 // Tells Ember to detect when app URl matches '/'
+Todo.TodosRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('todo');
+	}
+});
