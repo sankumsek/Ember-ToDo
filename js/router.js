8add04f7414428f8fcac6d8d249f7ecbc885e3b3
Sanjeev Sekar <sankumsek@gmail.com>
2014-12-30 22:27:27 -0800

    Fix errors regarding router

--- js/router.js
+++ js/router.js
@@ -2,7 +2,7 @@ Todos.Router.map(function() {
 	this.resource('todos', {path: '/' })
 });
 // Tells Ember to detect when app URl matches '/'
-Todo.TodosRoute = Ember.Route.extend({
+Todos.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
 	}
