7f79dc2c941d648d024ae550d05255ce1668d4fb
Sanjeev Sekar <sankumsek@gmail.com>
2015-01-03 21:27:50 -0800

    Create new router path

--- js/router.js
+++ js/router.js
@@ -1,9 +1,17 @@
 Todos.Router.map(function() {
-	this.resource('todos', {path: '/' })
+	this.resource('todos', {path: '/' }, function(){
+
+	});
 });
 // Tells Ember to detect when app URl matches '/'
 Todos.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('todos');
 	}
 });
